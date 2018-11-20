ad1fe936603c320c13c2d25dd19ce2757e9f2654
easywaldo <acetious@gmail.com>
2018-11-21 07:57:19 +0900

    [WIP] movie array implemented

--- src/App.js
+++ src/App.js
@@ -10,22 +10,38 @@ const movies = [
   "Expandable"
 ]
 
-const movieImages = [
-  "https://images-na.ssl-images-amazon.com/images/I/A1aHRPvn5JL._RI_.jpg"
-  , "https://images.amcnetworks.com/ifc.com/wp-content/uploads/2010/07/07202010_expendables1.jpg"
-  , "https://images-na.ssl-images-amazon.com/images/I/515wjJQt2nL._SY445_.jpg"
-  , "https://images-na.ssl-images-amazon.com/images/I/51vpnbwFHrL._SY445_.jpg"
+const movieList = [
+  {
+    title : "Matrix",
+    poster : "https://images-na.ssl-images-amazon.com/images/I/A1aHRPvn5JL._RI_.jpg"
+  },
+  {
+    title : "Infitiny War",
+    poster : "https://images.amcnetworks.com/ifc.com/wp-content/uploads/2010/07/07202010_expendables1.jpg"
+  },
+  {
+    title : "Iron Man",
+    poster : "https://images-na.ssl-images-amazon.com/images/I/515wjJQt2nL._SY445_.jpg"
+  },
+  {
+    title : "Expandable",
+    poster : "https://images-na.ssl-images-amazon.com/images/I/51vpnbwFHrL._SY445_.jpg"
+  }
 ]
-
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Movie title={movies[0]} poster={movieImages[0]}></Movie>
-        <Movie title={movies[1]} poster={movieImages[1]}></Movie>
-        <Movie title={movies[2]} poster={movieImages[2]}></Movie>
-        <Movie title={movies[3]} poster={movieImages[3]}></Movie>
+        {/* <Movie title={movieList[0].title} poster={movieList[0].poster}></Movie>
+        <Movie title={movieList[1].title} poster={movieList[1].poster}></Movie>
+        <Movie title={movieList[2].title} poster={movieList[2].poster}></Movie>
+        <Movie title={movieList[3].title} poster={movieList[3].poster}></Movie> */}
+        {
+          movieList.map(movie => () =>{
+            <Movie title={movie.title} poster={movie.poster}></Movie>
+        })
+        }
       </div>
     );
   }
