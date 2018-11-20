59d3ceff1dde52fb9f4806d360cd88e46a2f0579
easywaldo <acetious@gmail.com>
2018-11-21 07:39:17 +0900

    movie image implemented

--- src/App.js
+++ src/App.js
@@ -3,14 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 import Movie from './Movie';
 
+const movies = [
+  "Matrix",
+  "InfinityWar",
+  "Iron Man",
+  "Expandable"
+]
+
+const movieImages = [
+  "https://images-na.ssl-images-amazon.com/images/I/A1aHRPvn5JL._RI_.jpg"
+  , "https://images.amcnetworks.com/ifc.com/wp-content/uploads/2010/07/07202010_expendables1.jpg"
+  , "https://images-na.ssl-images-amazon.com/images/I/515wjJQt2nL._SY445_.jpg"
+  , "https://images-na.ssl-images-amazon.com/images/I/51vpnbwFHrL._SY445_.jpg"
+]
+
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Movie></Movie>
-        <Movie></Movie>
-        <Movie></Movie>
-        <Movie></Movie>
+        <Movie title={movies[0]} poster={movieImages[0]}></Movie>
+        <Movie title={movies[1]} poster={movieImages[1]}></Movie>
+        <Movie title={movies[2]} poster={movieImages[2]}></Movie>
+        <Movie title={movies[3]} poster={movieImages[3]}></Movie>
       </div>
     );
   }
--- src/Movie.js
+++ src/Movie.js
@@ -3,10 +3,11 @@ import './Movie.css';
 
 class Movie extends Component{
     render(){
+        //console.log(this.props);
         return(
             <div>
-                <h1>Hello this is a movie</h1>
-                <MoviePoster></MoviePoster>
+                <h1>{this.props.title}</h1>
+                <MoviePoster poster={this.props.poster}></MoviePoster>
             </div>
         );
     }
@@ -14,9 +15,9 @@ class Movie extends Component{
 
 class MoviePoster extends Component{
     render(){
+        console.log(this.props);
         return(
-            <img alt="" src="https://i.kinja-img.com/gawker-media/image/upload/s--JW1AWB8T--/c_fill,fl_progressive,g_center,h_264,q_80,w_470/spy5tadlmeeptys0gwks.jpg">
-            </img>
+            <img alt="" src={this.props.poster}></img>
         );
     }
 }
