45016e3373f9cf359b971eccd2839ebd913ce7f5
jn.lee <jn.lee@wconcept.co.kr>
2018-11-22 13:35:57 +0900

    setState 스터디
    setTimeout 을 이용하여 일정시간 뒤에 현재 state 에 새로운 영화정보를 추가한다.

--- src/App.js
+++ src/App.js
@@ -12,22 +12,18 @@ const movies = [
 
 const movieList = [
   {
-    id : 1,
     title : "Matrix",
     poster : "https://images-na.ssl-images-amazon.com/images/I/A1aHRPvn5JL._RI_.jpg"
   },
   {
-    id : 2,
     title : "Infitiny War",
     poster : "https://images.amcnetworks.com/ifc.com/wp-content/uploads/2010/07/07202010_expendables1.jpg"
   },
   {
-    id : 3,
     title : "Iron Man",
     poster : "https://images-na.ssl-images-amazon.com/images/I/515wjJQt2nL._SY445_.jpg"
   },
   {
-    id : 4,
     title : "Expandable",
     poster : "https://images-na.ssl-images-amazon.com/images/I/51vpnbwFHrL._SY445_.jpg"
   }
@@ -36,7 +32,24 @@ const movieList = [
 class App extends Component {
 
   state = {
-    greeting: "Hello"
+    movies: [
+      {
+        title : "Matrix",
+        poster : "https://images-na.ssl-images-amazon.com/images/I/A1aHRPvn5JL._RI_.jpg"
+      },
+      {
+        title : "Infitiny War",
+        poster : "https://images.amcnetworks.com/ifc.com/wp-content/uploads/2010/07/07202010_expendables1.jpg"
+      },
+      {
+        title : "Iron Man",
+        poster : "https://images-na.ssl-images-amazon.com/images/I/515wjJQt2nL._SY445_.jpg"
+      },
+      {
+        title : "Expandable",
+        poster : "https://images-na.ssl-images-amazon.com/images/I/51vpnbwFHrL._SY445_.jpg"
+      }
+    ]
   }
 
   componentWillMount(){
@@ -46,22 +59,27 @@ class App extends Component {
   componentDidMount(){
     setTimeout(() => {
       this.setState({
-        greeting: "Hello again"
+        movies: [
+          ...this.state.movies,
+          {
+            title : "kung fu panda",
+            poster : "https://upload.wikimedia.org/wikipedia/en/thumb/7/76/Kungfupanda.jpg/220px-Kungfupanda.jpg"
+          },
+        ]
       })
-    },5000);
+    }, 3000);
   }
 
   render() {
     console.log('did render');
     return (
       <div className="App">
-        {this.state.greeting}
         {/* <Movie title={movieList[0].title} poster={movieList[0].poster}></Movie>
         <Movie title={movieList[1].title} poster={movieList[1].poster}></Movie>
         <Movie title={movieList[2].title} poster={movieList[2].poster}></Movie>
         <Movie title={movieList[3].title} poster={movieList[3].poster}></Movie> */}
         {
-          movieList.map((movie, index) => {
+          this.state.movies.map((movie, index) => {
             return <Movie title={movie.title} poster={movie.poster} key={index}></Movie>
         })
         }
