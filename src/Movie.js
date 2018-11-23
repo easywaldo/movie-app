4ae55d5aa736c09abc4fca0b52b92ac78b731b51
easywaldo <acetious@gmail.com>
2018-11-23 22:51:53 +0900

    stateless component
    function 으로  props , state 등을 활용하지 않고
    html markup 만 리턴을 해주는 용도로 사용한다

--- src/Movie.js
+++ src/Movie.js
@@ -2,35 +2,57 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
 
-class Movie extends Component{
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        poster: PropTypes.string.isRequired
-    }
+// class Movie extends Component{
+//     static propTypes = {
+//         title: PropTypes.string.isRequired,
+//         poster: PropTypes.string.isRequired
+//     }
 
-    render(){
-        //console.log(this.props);
-        return(
-            <div>
-                <h1>{this.props.title}</h1>
-                <MoviePoster poster={this.props.poster}></MoviePoster>
-            </div>
-        );
-    }
+//     render(){
+//         //console.log(this.props);
+//         return(
+//             <div>
+//                 <h1>{this.props.title}</h1>
+//                 <MoviePoster poster={this.props.poster}></MoviePoster>
+//             </div>
+//         );
+//     }
+// }
+
+// class MoviePoster extends Component{
+
+//     static propTypes = {
+//         poster : PropTypes.string.isRequired
+//     }
+
+//     render(){
+//         console.log(this.props);
+//         return(
+//             <img alt="Movie Poster" src={this.props.poster}></img>
+//         );
+//     }
+// }
+function Movie({title, poster}) {
+    return (
+        <div>
+            <MoviePoster poster={poster} />
+            <h1>{title}</h1>
+        </div>
+    )
+}
+function MoviePoster({poster}) {
+    return (
+        <img alt="Movie Poster" src={poster}></img>
+    )
 }
 
-class MoviePoster extends Component{
+Movie.propTypes = {
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired
+}
 
-    static propTypes = {
-        poster : PropTypes.string.isRequired
-    }
-
-    render(){
-        console.log(this.props);
-        return(
-            <img alt="Movie Poster" src={this.props.poster}></img>
-        );
-    }
+MoviePoster.propTypes = {
+    poster: PropTypes.string.isRequired
 }
 
 export default Movie;
