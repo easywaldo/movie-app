cac9a30b8fd26f4865948a85c16b56a32527a500
easywaldo <acetious@gmail.com>
2018-11-24 17:08:05 +0900

    fetch 와 then 을 이용하여 데이터를 가져오도록 한다.
    api 호출을 비동기로 호출하도록 하기 위해
    async 와 await 를 이용한다.
    
    그외에 css 를 적용함.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
 import Movie from './Movie';
-
-const movies = [
-  "Matrix",
-  "InfinityWar",
-  "Iron Man",
-  "Expandable"
-]
-
-const movieList = [
-  {
-    title : "Matrix",
-    poster : "https://images-na.ssl-images-amazon.com/images/I/A1aHRPvn5JL._RI_.jpg"
-  },
-  {
-    title : "Infitiny War",
-    poster : "https://images.amcnetworks.com/ifc.com/wp-content/uploads/2010/07/07202010_expendables1.jpg"
-  },
-  {
-    title : "Iron Man",
-    poster : "https://images-na.ssl-images-amazon.com/images/I/515wjJQt2nL._SY445_.jpg"
-  },
-  {
-    title : "Expandable",
-    poster : "https://images-na.ssl-images-amazon.com/images/I/51vpnbwFHrL._SY445_.jpg"
-  }
-]
+import './App.css';
+import './Movie.css';
 
 class App extends Component {
 
@@ -39,57 +14,43 @@ class App extends Component {
   }
   
   componentDidMount(){
-    setTimeout(() => {
-      this.setState({
-        movies: [
-          {
-            title : "Matrix",
-            poster : "https://images-na.ssl-images-amazon.com/images/I/A1aHRPvn5JL._RI_.jpg"
-          },
-          {
-            title : "Infitiny War",
-            poster : "https://images.amcnetworks.com/ifc.com/wp-content/uploads/2010/07/07202010_expendables1.jpg"
-          },
-          {
-            title : "Iron Man",
-            poster : "https://images-na.ssl-images-amazon.com/images/I/515wjJQt2nL._SY445_.jpg"
-          },
-          {
-            title : "Expandable",
-            poster : "https://images-na.ssl-images-amazon.com/images/I/51vpnbwFHrL._SY445_.jpg"
-          },
-          {
-            title : "kung fu panda",
-            poster : "https://upload.wikimedia.org/wikipedia/en/thumb/7/76/Kungfupanda.jpg/220px-Kungfupanda.jpg"
-          },
-        ]
-      })
-    }, 3000);
+    this._getMovies();
+  }
+
+  _getMovies = async () => {
+    const movies = await this._callApi();
+    this.setState({
+      movies
+    }) 
+  }
+
+  _callApi = () => {
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=download_count')
+    .then(response => response.json())
+    .then(json => json.data.movies)
+    .catch(err => console.log(err))
   }
 
   _renderMovies = () => {
-    const movies = this.state.movies.map((movie, index) => {
-      return <Movie title={movie.title} poster={movie.poster} key={index}></Movie>
+    const movies = this.state.movies.map(movie => {
+      console.log(movie);
+      return <Movie 
+          title={movie.title_english}
+          poster={movie.medium_cover_image}
+          key={movie.id}
+          genres={movie.genres}
+          synopsis={movie.synopsis}>
+        </Movie>
     });
     return movies;
   }
 
   render() {
     console.log('did render');
+    const { movies } = this.state;
     return (
-      <div className="App">
-        {/* <Movie title={movieList[0].title} poster={movieList[0].poster}></Movie>
-        <Movie title={movieList[1].title} poster={movieList[1].poster}></Movie>
-        <Movie title={movieList[2].title} poster={movieList[2].poster}></Movie>
-        <Movie title={movieList[3].title} poster={movieList[3].poster}></Movie> */}
-        {/* {
-          this.state.movies.map((movie, index) => {
-            return <Movie title={movie.title} poster={movie.poster} key={index}></Movie>
-        })
-        } */}
-        {
-          this.state.movies ? this._renderMovies() : 'Loading'
-        }
+      <div className={movies ? "App" : "App-loading"}>
+        {movies ? this._renderMovies() : 'Loading'}
       </div>
     );
   }
